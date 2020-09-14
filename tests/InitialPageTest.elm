7081cff524b57f70622bbc403f2fc52dc154f211
Marcio Frayze <mfdavid@gmail.com>
2020-09-14 13:23:43 -0300

    Restructuing tests

--- tests/InitialPageTest.elm
+++ tests/InitialPageTest.elm
@@ -16,96 +16,102 @@ suite =
             view emptyMeeting
                 |> Query.fromHtml
     in
-    describe "User opens initial page"
-        [ test "should see a title" <|
-            \_ ->
-                emptyMeetingHtml
-                    |> Query.find [ Selector.id "title" ]
-                    |> Query.has [ Selector.text "Meeting price counter" ]
-        , test "should see an enabled 'Start counting' button" <|
-            \_ ->
-                emptyMeetingHtml
-                    |> Query.has
-                        [ Selector.id "pauseButton"
-                        , Selector.disabled True
-                        , Selector.text "Start counting"
-                        ]
-        , test "should see a disabled 'Pause' button" <|
-            \_ ->
-                emptyMeetingHtml
-                    |> Query.has
-                        [ Selector.id "pauseButton"
-                        , Selector.disabled False
-                        , Selector.text "Pause"
-                        ]
-        , test "should see a disabled 'Reset' button" <|
-            \_ ->
-                emptyMeetingHtml
-                    |> Query.has
-                        [ Selector.id "resetButton"
-                        , Selector.disabled True
-                        , Selector.text "Reset"
-                        ]
-        , test "should see the time elapsed as 0" <|
-            \_ ->
-                emptyMeetingHtml
-                    |> Query.find [ Selector.id "timeElapsed" ]
-                    |> Query.has [ Selector.text "0" ]
-        , test "should see the amount spent as 0" <|
-            \_ ->
-                emptyMeetingHtml
-                    |> Query.find [ Selector.id "amountSpent" ]
-                    |> Query.has [ Selector.text "0" ]
-        , test "should see a title for the selector" <|
-            \_ ->
-                emptyMeetingHtml
-                    |> Query.find [ Selector.id "averageSalary" ]
-                    |> Query.has [ Selector.text "Whats is the average salary per month per participant?" ]
-        , test "should see an average salary select" <|
-            \_ ->
-                emptyMeetingHtml
-                    |> Query.find [ Selector.id "averageSalarySelect", Selector.disabled False ]
-                    |> Query.has
-                        (List.map
-                            (\value ->
-                                Selector.text
-                                    ("$" ++ value ++ " dollars")
+    describe "On initial page"
+        [ describe "user should see"
+            [ test "a title" <|
+                \_ ->
+                    emptyMeetingHtml
+                        |> Query.find [ Selector.id "title" ]
+                        |> Query.has [ Selector.text "Meeting price counter" ]
+            , test "an enabled 'Start counting' button" <|
+                \_ ->
+                    emptyMeetingHtml
+                        |> Query.has
+                            [ Selector.id "pauseButton"
+                            , Selector.disabled True
+                            , Selector.text "Start counting"
+                            ]
+            , test "a disabled 'Pause' button" <|
+                \_ ->
+                    emptyMeetingHtml
+                        |> Query.has
+                            [ Selector.id "pauseButton"
+                            , Selector.disabled False
+                            , Selector.text "Pause"
+                            ]
+            , test "a disabled 'Reset' button" <|
+                \_ ->
+                    emptyMeetingHtml
+                        |> Query.has
+                            [ Selector.id "resetButton"
+                            , Selector.disabled True
+                            , Selector.text "Reset"
+                            ]
+            , test "the time elapsed as 0" <|
+                \_ ->
+                    emptyMeetingHtml
+                        |> Query.find [ Selector.id "timeElapsed" ]
+                        |> Query.has [ Selector.text "0" ]
+            , test "the amount spent as 0" <|
+                \_ ->
+                    emptyMeetingHtml
+                        |> Query.find [ Selector.id "amountSpent" ]
+                        |> Query.has [ Selector.text "0" ]
+            , test "a title for the average salary selector" <|
+                \_ ->
+                    emptyMeetingHtml
+                        |> Query.find [ Selector.id "averageSalary" ]
+                        |> Query.has [ Selector.text "Whats is the average salary per month per participant?" ]
+            , test "an average salary select" <|
+                \_ ->
+                    emptyMeetingHtml
+                        |> Query.find [ Selector.id "averageSalarySelect", Selector.disabled False ]
+                        |> Query.has
+                            (List.map
+                                (\value ->
+                                    Selector.text
+                                        ("$" ++ value ++ " dollars")
+                                )
+                                [ "0", "300", "500", "1000", "2000", "4000", "6000", "8000", "1000", "13000" ]
                             )
-                            [ "0", "300", "500", "1000", "2000", "4000", "6000", "8000", "1000", "13000" ]
-                        )
-        , test "average salary per month per atendee should be 0" <|
-            \_ ->
-                Expect.equal emptyMeeting.averageSalaryPerMonthPerAtendee 0
-        , test "amount spent in the meeting should be 0" <|
-            \_ ->
-                Expect.equal emptyMeeting.amountSpent 0
-        , test "timer shoud be Paused" <|
-            \_ ->
-                Expect.equal emptyMeeting.timerStatus Paused
-        , describe "when clicking 'Start counting'"
-            [ test "should dispatch StartCounting" <|
-                \_ ->
-                    emptyMeetingHtml
-                        |> Query.find [ Selector.id "startButton" ]
-                        |> Event.simulate Event.click
-                        |> Event.expect StartCounting
             ]
-        , describe "when selecting an average 'Start counting'"
-            [ test "should dispatch StartCounting" <|
+        , describe "in the initial model"
+            [ test "average salary per month should be 0" <|
                 \_ ->
-                    emptyMeetingHtml
-                        |> Query.find [ Selector.id "startButton" ]
-                        |> Event.simulate Event.click
-                        |> Event.expect StartCounting
+                    Expect.equal emptyMeeting.averageSalaryPerMonthPerAtendee 0
+            , test "amount spent in the meeting should be 0" <|
+                \_ ->
+                    Expect.equal emptyMeeting.amountSpent 0
+            , test "timer shoud be Paused" <|
+                \_ ->
+                    Expect.equal emptyMeeting.timerStatus Paused
             ]
+        , describe "user actions"
+            [ describe "when clicking 'Start counting'"
+                [ test "should dispatch StartCounting" <|
+                    \_ ->
+                        emptyMeetingHtml
+                            |> Query.find [ Selector.id "startButton" ]
+                            |> Event.simulate Event.click
+                            |> Event.expect StartCounting
+                ]
+            , describe "when selecting an average 'Start counting'"
+                [ test "should dispatch StartCounting" <|
+                    \_ ->
+                        emptyMeetingHtml
+                            |> Query.find [ Selector.id "startButton" ]
+                            |> Event.simulate Event.click
+                            |> Event.expect StartCounting
+                ]
 
-        -- TODO: Don't know how to simulate this, yet
-        -- , describe "when selecting last average salary"
-        --     [ test "should dispatch AverageSalarySelected with 30000 value" <|
-        --         \_ ->
-        --             emptyMeetingHtml
-        --                 |> Query.find [ Selector.id "averageSalarySelect" ]
-        --                 |> Event.simulate Event.click
-        --                 |> Event.expect AverageSalarySelected 30000
-        --     ]
+            -- TODO: Don't know how to simulate this, yet
+            -- , describe "when selecting last average salary"
+            --     [ test "should dispatch AverageSalarySelected with 30000 value" <|
+            --         \_ ->
+            --             emptyMeetingHtml
+            --                 |> Query.find [ Selector.id "averageSalarySelect" ]
+            --                 |> Event.simulate Event.click
+            --                 |> Event.expect AverageSalarySelected 30000
+            --     ]
+            ]
         ]
